654b04e9cbfb41271821905f00cef07c7d4162b9
effe <xieyu567@hotmail.com>
2024-05-06 23:05:06 +0800

    refactor test

--- script/DeployFundMe.s.sol
+++ script/DeployFundMe.s.sol
@@ -6,11 +6,9 @@ import {Script} from "forge-std/Script.sol";
 import {FundMe} from "../src/FundMe.sol";
 
 contract DeployFundMe is Script {
-    address public constant USER = address(1);
-
     function run() external returns (FundMe) {
         vm.startBroadcast();
-        FundMe fundMe = new FundMe(USER);
+        FundMe fundMe = new FundMe(0x694AA1769357215DE4FAC081bf1f309aDC325306);
         vm.stopBroadcast();
         return (fundMe);
     }
--- test/FundMe.t.sol
+++ test/FundMe.t.sol
@@ -4,17 +4,23 @@ pragma solidity ^0.8.25;
 
 import {Test, console} from "forge-std/Test.sol";
 import {FundMe} from "../src/FundMe.sol";
+import {DeployFundMe} from "script/DeployFundMe.s.sol";
 
 contract FundMeTest is Test {
     FundMe public fundMe;
 
-    address public constant USER = address(1);
-
     function setUp() external {
-        fundMe = new FundMe(USER);
+        DeployFundMe deployFundMe = new DeployFundMe();
+        fundMe = deployFundMe.run();
     }
 
     function testMinDollor() public view {
         assertEq(fundMe.MINIMUM_USD(), 5 * 10 ** 18);
+    }
+
+    function testPriceFeedVersion() public view {
+        uint256 version = fundMe.getVersion();
+        console.log(version);
+        assertEq(version, 4);
     }
 }
